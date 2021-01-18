3e99ab6ffc91e1b38f4c67924c337a016ea28e6e
stephanie-calmels <stephcalmels82@gmail.com>
2021-01-18 17:35:55 +0100

    ADD react-model + modules router redux axios leaflet slugify semantic-ui

---
+++ Front/src/components/App/index.js
@@ -0,0 +1,15 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './styles.scss';
+
+// == Composant
+const App = () => {
+  return <div className="app">
+
+  </div>
+};
+
+// == Export
+export default App;
