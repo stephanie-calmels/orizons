60b82821565888a607d5366093dd402ddf4511b5
stephanie-calmels <stephcalmels82@gmail.com>
2021-01-18 18:04:06 +0100

    ADD seed trips and members

---
+++ Front/src/data/trips.js
@@ -0,0 +1,427 @@
+export default [
+  {
+    id: 1,
+    title: 'Traversée des Alpes',
+    summary: '7 jours de randonnée à travers le massif des Alpes',
+    departure_date: '12-08-2017',
+    arrival_date: '19-08-2017',
+    duration: 7,
+    score: 3,
+    cover_photo: {
+      id: 100,
+      title: 'photo 100',
+      url: 'https://images.pexels.com/photos/1562/italian-landscape-mountains-nature.jpg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+      member_id: 1
+    },
+    localisation: [
+      'France'
+    ],
+    categories: [
+      {
+        id: 1,
+        entitled: 'Randonnée',
+        color: '#59F19C'
+      }
+    ],
+    author: {
+      id: 1,
+      first_name: 'Nicole',
+      last_name: 'Hincham',
+      nickname: 'Nicole',
+      email: 'nhincham0@hugedomains.com',
+      password: 'akFIz6qPqYA',
+      profile_photo: {
+        id: 101,
+        title: 'photo 101',
+        url: '',
+        member_id: 1
+      },
+      registration_date: '04-07-2016',
+      cover_photo: {
+        id: 102,
+        title: 'photo 102',
+        url: '',
+        member_id: 1
+      },
+      localisation: 'Belgique'
+    },
+    steps: [
+      {
+        id: 1,
+        longitude: '',
+        latitude: '',
+        title: 'Jour 1',
+        date: '12-08-2017',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sollicitudin egestas pharetra. Duis non lectus hendrerit turpis tristique venenatis.',
+        photos: [
+          {
+            id: 1,
+            title: 'photo 1',
+            url: 'https://images.pexels.com/photos/1482927/pexels-photo-1482927.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            member_id: 1
+          }
+        ]
+      },
+      {
+        id: 2,
+        longitude: '',
+        latitude: '',
+        title: 'Jour 2',
+        date: '13-08-2017',
+        content: 'Donec gravida viverra neque quis vehicula. Quisque nec placerat tellus. Proin imperdiet ipsum vitae velit rhoncus volutpat. Duis eros turpis, ullamcorper et gravida vitae, fermentum vel ex. Morbi cursus purus ipsum, sed efficitur tellus dictum sed. Cras finibus sit amet justo id venenatis.',
+        photos: [
+          {
+            id: 2,
+            title: 'photo 2',
+            url: 'https://images.pexels.com/photos/358532/pexels-photo-358532.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            member_id: 1
+          }
+        ]
+      },
+      {
+        id: 3,
+        longitude: '',
+        latitude: '',
+        title: 'Jour 3',
+        date: '14-08-2017',
+        content: 'Etiam vel bibendum lectus. Pellentesque porttitor non lectus et mollis. Phasellus mollis, dolor ac suscipit iaculis, justo ligula pharetra ligula, ullamcorper pulvinar nulla nunc vel eros. ',
+        photos: [
+          {
+            id: 3,
+            title: 'photo 3',
+            url: 'https://images.pexels.com/photos/753325/pexels-photo-753325.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            member_id: 1
+          }
+        ]
+      },
+      {
+        id: 4,
+        longitude: '',
+        latitude: '',
+        title: 'Jour 4',
+        date: '15-08-2017',
+        content: 'Phasellus mollis, dolor ac suscipit iaculis, justo ligula pharetra ligula, ullamcorper pulvinar nulla nunc vel eros. Morbi lacinia turpis felis, et pulvinar urna suscipit sed.',
+        photos: [
+          {
+            id: 4,
+            title: 'photo 4',
+            url: 'https://images.pexels.com/photos/554609/pexels-photo-554609.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            member_id: 1
+          }
+        ]
+      },
+      {
+        id: 5,
+        longitude: '',
+        latitude: '',
+        title: 'Jour 5',
+        date: '16-08-2017',
+        content: 'tiam sollicitudin egestas pharetra. Duis non lectus hendrerit turpis tristique venenatis. Donec gravida viverra neque quis vehicula. Quisque nec placerat tellus. Proin imperdiet ipsum vitae velit rhoncus volutpat. Duis eros turpis, ullamcorper et gravida vitae, fermentum vel ex.',
+        photos: [
+          {
+            id: 5,
+            title: 'photo 5',
+            url: 'https://images.pexels.com/photos/544067/pexels-photo-544067.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            member_id: 1
+          }
+        ]
+      },
+      {
+        id: 6,
+        longitude: '',
+        latitude: '',
+        title: 'Jour 6',
+        date: '17-08-2017',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sollicitudin egestas pharetra. Duis non lectus hendrerit turpis tristique venenatis. Donec gravida viverra neque quis vehicula. ',
+        photos: [
+          {
+            id: 6,
+            title: 'photo 6',
+            url: 'https://images.pexels.com/photos/267104/pexels-photo-267104.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            member_id: 1
+          }
+        ]
+      },
+      {
+        id: 7,
+        longitude: '',
+        latitude: '',
+        title: 'Jour 7',
+        date: '18-08-2017',
+        content: 'Morbi cursus purus ipsum, sed efficitur tellus dictum sed. Cras finibus sit amet justo id venenatis. Etiam vel bibendum lectus. Pellentesque porttitor non lectus et mollis. Phasellus mollis, dolor ac suscipit iaculis, justo ligula pharetra ligula, ullamcorper pulvinar nulla nunc vel eros.',
+        photos: [
+          {
+            id: 7,
+            title: 'photo 7',
+            url: 'https://images.pexels.com/photos/552766/pexels-photo-552766.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            member_id: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: 'Bora-Bora',
+    summary: 'Vacances détente sur les plages de Polynésie française',
+    departure_date: '06-06-2020',
+    arrival_date: '13-06-2020',
+    duration: 7,
+    score: 7,
+    cover_photo: {
+      id: 120,
+      title: 'photo 120',
+      url: 'https://images.pexels.com/photos/753626/pexels-photo-753626.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      member_id: 2
+    },
+    localisation: [
+      'Polynésie française'
+    ],
+    categories: [
+      {
+        id: 2,
+        entitled: 'Farniente',
+        color: '#FEFA85'
+      }
+    ],
+    author: {
+      id: 2,
+      first_name: 'Michel',
+      last_name: 'Easseby',
+      nickname: 'Michel',
+      email: 'measseby1@google.fr',
+      password: 'ooGFz6uDU',
+      profile_photo: {
+        id: 111,
+        title: 'photo 111',
+        url: '',
+        member_id: 2
+      },
+      registration_date: '27-05-2004',
+      cover_photo: {
+        id: 112,
+        title: 'photo 112',
+        url: '',
+        member_id: 2
+      },
+      localisation: 'Philippines'
+    },
+    steps: [
+      {
+        id: 10,
+        longitude: '',
+        latitude: '',
+        title: 'Jour 1',
+        date: '06-06-2020',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sollicitudin egestas pharetra. Duis non lectus hendrerit turpis tristique venenatis.',
+        photos: [
+          {
+            id: 11,
+            title: 'photo 1',
+            url: 'https://images.pexels.com/photos/1188473/pexels-photo-1188473.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            member_id: 2
+          }
+        ]
+      },
+      {
+        id: 20,
+        longitude: '',
+        latitude: '',
+        title: 'Jour 2',
+        date: '07-06-2020',
+        content: 'Donec gravida viverra neque quis vehicula. Quisque nec placerat tellus. Proin imperdiet ipsum vitae velit rhoncus volutpat. Duis eros turpis, ullamcorper et gravida vitae, fermentum vel ex. Morbi cursus purus ipsum, sed efficitur tellus dictum sed. Cras finibus sit amet justo id venenatis.',
+        photos: [
+          {
+            id: 12,
+            title: 'photo 2',
+            url: 'https://images.pexels.com/photos/1188470/pexels-photo-1188470.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            member_id: 2
+          }
+        ]
+      },
+      {
+        id: 30,
+        longitude: '',
+        latitude: '',
+        title: 'Jour 3',
+        date: '08-06-2020',
+        content: 'Etiam vel bibendum lectus. Pellentesque porttitor non lectus et mollis. Phasellus mollis, dolor ac suscipit iaculis, justo ligula pharetra ligula, ullamcorper pulvinar nulla nunc vel eros. ',
+        photos: [
+          {
+            id: 13,
+            title: 'photo 3',
+            url: 'https://images.pexels.com/photos/1021068/pexels-photo-1021068.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            member_id: 2
+          }
+        ]
+      },
+      {
+        id: 40,
+        longitude: '',
+        latitude: '',
+        title: 'Jour 4',
+        date: '09-06-2020',
+        content: 'Phasellus mollis, dolor ac suscipit iaculis, justo ligula pharetra ligula, ullamcorper pulvinar nulla nunc vel eros. Morbi lacinia turpis felis, et pulvinar urna suscipit sed.',
+        photos: [
+          {
+            id: 14,
+            title: 'photo 4',
+            url: 'https://images.pexels.com/photos/3355732/pexels-photo-3355732.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            member_id: 2
+          }
+        ]
+      },
+      {
+        id: 50,
+        longitude: '',
+        latitude: '',
+        title: 'Jour 5',
+        date: '10-06-2020',
+        content: 'tiam sollicitudin egestas pharetra. Duis non lectus hendrerit turpis tristique venenatis. Donec gravida viverra neque quis vehicula. Quisque nec placerat tellus. Proin imperdiet ipsum vitae velit rhoncus volutpat. Duis eros turpis, ullamcorper et gravida vitae, fermentum vel ex.',
+        photos: [
+          {
+            id: 15,
+            title: 'photo 5',
+            url: 'https://images.pexels.com/photos/3250613/pexels-photo-3250613.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            member_id: 2
+          }
+        ]
+      },
+      {
+        id: 60,
+        longitude: '',
+        latitude: '',
+        title: 'Jour 6',
+        date: '11-06-2020',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sollicitudin egestas pharetra. Duis non lectus hendrerit turpis tristique venenatis. Donec gravida viverra neque quis vehicula. ',
+        photos: [
+          {
+            id: 16,
+            title: 'photo 6',
+            url: 'https://images.pexels.com/photos/3390587/pexels-photo-3390587.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            member_id: 2
+          }
+        ]
+      },
+      {
+        id: 70,
+        longitude: '',
+        latitude: '',
+        title: 'Jour 7',
+        date: '12-06-2020',
+        content: 'Morbi cursus purus ipsum, sed efficitur tellus dictum sed. Cras finibus sit amet justo id venenatis. Etiam vel bibendum lectus. Pellentesque porttitor non lectus et mollis. Phasellus mollis, dolor ac suscipit iaculis, justo ligula pharetra ligula, ullamcorper pulvinar nulla nunc vel eros.',
+        photos: [
+          {
+            id: 17,
+            title: 'photo 7',
+            url: 'https://images.pexels.com/photos/2233368/pexels-photo-2233368.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            member_id: 2
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: 3,
+    title: 'Stonehenge',
+    summary: 'Petit week-end romantique avec Guenièvre',
+    departure_date: '01-07-2005',
+    arrival_date: '02-07-2005',
+    duration: 2,
+    score: 10,
+    cover_photo: {
+      id: 130,
+      title: 'photo 130',
+      url: 'https://images.pexels.com/photos/1448136/pexels-photo-1448136.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      member_id: 3
+    },
+    localisation: [
+      'Angleterre'
+    ],
+    categories: [
+      {
+        id: 1,
+        entitled: 'Randonnée',
+        color: '#59F19C'
+      },
+      {
+        id: 3,
+        entitled: 'Week-end',
+        color: '#FA85FE'
+      }
+    ],
+    author: {
+      id: 3,
+      first_name: 'Arthur',
+      last_name: 'Pendragon',
+      email: 'kingarthur@google.com',
+      password: 'sjgoaZvkA2xk',
+      profile_photo: {
+        id: 131,
+        title: 'photo 131',
+        url: '',
+        member_id: 3
+      },
+      registration_date: '17-03-2002',
+      cover_photo: {
+        id: 132,
+        title: 'photo 132',
+        url: '',
+        member_id: 3
+      },
+      localisation: 'Angleterre'
+    },
+    steps: [
+      {
+        id: 80,
+        longitude: '',
+        latitude: '',
+        title: 'Jour 1',
+        date: '01-07-500',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sollicitudin egestas pharetra. Duis non lectus hendrerit turpis tristique venenatis.',
+        photos: [
+          {
+            id: 80,
+            title: 'photo 1',
+            url: 'https://images.pexels.com/photos/3233007/pexels-photo-3233007.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            member_id: 3
+          },
+          {
+            id: 81,
+            title: 'photo 2',
+            url: 'https://images.pexels.com/photos/162056/pexels-photo-162056.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            member_id: 3
+          }
+        ]
+      },
+      {
+        id: 90,
+        longitude: '',
+        latitude: '',
+        title: 'Jour 2',
+        date: '02-07-500',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sollicitudin egestas pharetra. Duis non lectus hendrerit turpis tristique venenatis.',
+        photos: [
+          {
+            id: 82,
+            title: 'photo 3',
+            url: 'https://images.pexels.com/photos/1590511/pexels-photo-1590511.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            member_id: 3
+          },
+          {
+            id: 83,
+            title: 'photo 4',
+            url: 'https://images.pexels.com/photos/941229/pexels-photo-941229.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            member_id: 3
+          },
+          {
+            id: 84,
+            title: 'photo 5',
+            url: 'https://images.pexels.com/photos/1756325/pexels-photo-1756325.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            member_id: 3
+          }
+        ]
+      }
+    ]
+  }
+];
