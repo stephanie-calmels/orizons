70f0253745e8d2bcf9498751b868adf1decab1d1
Armandine BARGE <armandine.barge@ntymail.com>
2021-01-18 11:46:07 +0100

    npm, sqitch and bdd initialization

---
+++ Back/migration/revert/0000-init.sql
@@ -0,0 +1,17 @@
+-- Revert projet-orizons:0000-init from pg
+
+BEGIN;
+
+
+DROP TABLE IF EXISTS "_m2m_trip_category" CASCADE;
+DROP TABLE IF EXISTS "_m2m_trip_localisation" CASCADE;
+DROP TABLE IF EXISTS "step" CASCADE;
+DROP TABLE IF EXISTS "comment" CASCADE;
+DROP TABLE IF EXISTS "category" CASCADE;
+DROP TABLE IF EXISTS "trip" CASCADE;
+DROP TABLE IF EXISTS "user" CASCADE;
+DROP TABLE IF EXISTS "photo" CASCADE;
+DROP TABLE IF EXISTS "localisation" CASCADE;
+
+
+COMMIT;
---
+++ Back/migration/verify/0000-init.sql
@@ -0,0 +1,7 @@
+-- Verify projet-orizons:0000-init on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
