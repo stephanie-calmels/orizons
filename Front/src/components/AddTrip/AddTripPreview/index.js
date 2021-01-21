05a920214e3a19edb73c023b52823f984339b045
paulcourtois <paulcourtoispaul@gmail.com>
2021-01-21 23:34:37 +0100

    ADD preview on desktop version, input file, date and checkbox ok

--- Front/src/components/AddTrip/AddTripPreview/index.js
+++ Front/src/components/AddTrip/AddTripPreview/index.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import {Card, Badge} from 'react-bootstrap';
 
-const AddTripPreview =()=>{
-  return <div></div>
+const AddTripPreview =({inputs})=>{
+  console.log(inputs)
+  return <div>
+    <Card className="card_trips">
+                  <Card.Img className="card_trips-img-top" variant="top" src={inputs.coverpicture} />
+                  <Card.Body className="card_trips-body">
+                  <Card.Title className="card_trips-title">{inputs.title}</Card.Title>
+                  {/*Corriger l'affichage de la date dans le bon sens */}
+                  <Card.Subtitle>{inputs.departure} {inputs.returndate && "jusqu'au"} {inputs.returndate}</Card.Subtitle>
+                  <Card.Subtitle>{inputs.localisation}</Card.Subtitle>
+                  <Card.Text className="card_trips-text">
+                      {inputs.summary}
+                  </Card.Text>
+                  <Card.Text className="card_trips-text">
+                    {inputs.categories.map(category => (
+                      
+                        <Badge pill key={category.id} className="tag" style={{backgroundColor: `${category.color}`}}>
+                          {category.entitled}
+                        </Badge>                 
+                    ))}
+                  </Card.Text>
+                  </Card.Body>
+                </Card>
+  </div>
 }
 
 export default AddTripPreview
--- Front/src/components/AddTrip/index.js
+++ Front/src/components/AddTrip/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import {
-  Container, Form, Button,
+  Container, Form, Button, Row, Col
 } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
@@ -8,6 +8,8 @@ import { useForm } from 'react-hook-form';
 import categoriesList from 'src/data/categories';
 
 import AddTripPreview from './AddTripPreview'
+
+import './addtrip.scss'
 
 const AddTrip = () => {
   const [inputs, setInputs] = useState({
@@ -17,142 +19,164 @@ const AddTrip = () => {
     categories: [],
     departure: '',
     returndate: '',
+    coverpicture
   });
 
   const {
-    title, summary, localisation, categories, departure, returndate
+    title, summary, localisation, categories, departure, returndate, coverpicture
   } = inputs;
 
   const handleChange = (e) => setInputs({ ...inputs, [e.target.name]: e.target.value });
 
   const handleCheckbox = (e)=> {
-    console.log(e.target.name)
-    
-    setInputs({...inputs, [e.target.name]: [...categories, e.target.value]})
+    // Je suis obligé de refaire un find ici, je ne pouvais pas récupérer l'objet catégorie depuis le formulaire, seulement une string
+    const clickedCategory = categoriesList.find(category => category.entitled == e.target.value)
+    const index = categories.indexOf(clickedCategory)
+    // Si index > -1, c'est à dire si notre clickedCategory existe déjà dans le state, alors on la supprime avec splice
+    if (index > -1){
+      categories.splice(index, 1);
+      return setInputs({...inputs, [e.target.name]:[...categories]});
+    }
+    // Sinon on l'ajoute au tableau des catégories
+    setInputs({...inputs, [e.target.name]: [...categories, clickedCategory]})
   }
-  // Hook qui vient de React Hook Form
-  // https://react-hook-form.com/get-started
+  
   const {
     register, handleSubmit, errors,
   } = useForm({});
-  // on cherche à voir si le serveur a bien reçu les infos
   const [submitting, setSubmitting] = useState(false);
+
 
   return <div>
     <h1>Créer un nouveau carnet</h1>
     <Container>
-      <Form
-            className="form-add-trip"
-            onSubmit={handleSubmit((formData) => {
-              // on récupère un objet avec toutes les données. Envoyées seulement si correctes
-              setSubmitting(true);
-              // eslint-disable-next-line no-console
-              console.log('formData', formData);
-              // TODO: requête AXIOS pour envoyer les infos au serveur
+      
+        <Form
+              className="form-add-trip"
+              onSubmit={handleSubmit((formData) => {
+                setSubmitting(true);
+                console.log('formData', formData);
+                // TODO: requête AXIOS pour envoyer les infos au serveur
 
-              setSubmitting(false);
-            })}
-          >
-        <Form.Group size="lg" controlId="title">
-            <Form.Label>Titre de votre voyage</Form.Label>
-            <Form.Control
-              autoFocus
-              name="title"
-              type="text"
-              defaultValue={title}
-              onChange={(e) => handleChange(e)}
-              // on attache notre input au React Hook Form pour les critères de validation
-              ref={register({
-                // si le champ n'est pas rempli lors de la soumission, le champ se met en focus
-                required: 'Veuillez remplir ce champ !',
+                setSubmitting(false);
               })}
-            />
-            {errors.title && <div className="text-danger">{errors.title.message}</div>}
-          </Form.Group>
-          <Form.Group size="lg" controlId="summary">
-            <Form.Label>Description de votre voyage</Form.Label>
-            <Form.Control
-              autoFocus
-              name="summary"
-              as="textarea"
-              rows={5}
-              defaultValue={summary}
-              onChange={(e) => handleChange(e)}
-              ref={register({
-                required: 'Veuillez remplir ce champ !',
+            >
+      <Row>
+      <Col sm={12} md={6} lg={4}>
+          <Form.Group size="lg" controlId="title">
+              <Form.Label>Titre de votre voyage</Form.Label>
+              <Form.Control
+                autoFocus
+                name="title"
+                type="text"
+                defaultValue={title}
+                onChange={(e) => handleChange(e)}
+                ref={register({
+                  required: 'Veuillez remplir ce champ !',
+                })}
+              />
+              {errors.title && <div className="text-danger">{errors.title.message}</div>}
+            </Form.Group>
+            <Form.Group size="lg" controlId="summary">
+              <Form.Label>Description de votre voyage</Form.Label>
+              <Form.Control
+                name="summary"
+                as="textarea"
+                rows={5}
+                defaultValue={summary}
+                onChange={(e) => handleChange(e)}
+                ref={register({
+                  required: 'Veuillez remplir ce champ !',
+                })}
+              />
+              {errors.summary && <div className="text-danger">{errors.summary.message}</div>}
+            </Form.Group>
+            <Form.Group size="lg" controlId="localisation">
+              <Form.Label>Localisation</Form.Label>
+              <Form.Control
+                name="localisation"
+                type="text"
+                defaultValue={localisation}
+                onChange={(e) => handleChange(e)}
+                ref={register({
+                  required: 'Veuillez remplir ce champ !',
+                })}
+              />
+              {errors.localisation && <div className="text-danger">{errors.localisation.message}</div>}
+            </Form.Group>
+            <Form.Group size="lg" controlId="coverpicture">
+              <Form.Label>Photo de couverture</Form.Label>
+              <Form.Control
+                name="coverpicture"
+                type="file"
+                defaultValue={coverpicture}
+                onChange={(e) => handleChange(e)}
+                ref={register({
+                  required: 'Veuillez sélectionner une photo',
+                })}
+              />
+              {errors.coverpicture && <div className="text-danger">{errors.coverpicture.message}</div>}
+            </Form.Group>
+          </Col>
+          <Col sm={12} md={6} lg={4}>
+            <Form.Group size="lg" controlId="categories">
+              <Form.Label>Style de votre voyage</Form.Label>
+              <div>
+              {categoriesList.map(category =>{
+                return <Form.Check
+                key={category.id}
+                type="checkbox"
+                label={category.entitled}
+                name="categories"
+                value={category.entitled}
+                onChange={(e) => handleCheckbox(e)}
+                ref={register({
+                  required: 'Veuillez sélectionner au moins une catégorie !',
+                })}
+              />
               })}
-            />
-            {errors.summary && <div className="text-danger">{errors.summary.message}</div>}
-          </Form.Group>
-          <Form.Group size="lg" controlId="localisation">
-            <Form.Label>Localisation</Form.Label>
-            <Form.Control
-              autoFocus
-              name="localisation"
-              type="text"
-              defaultValue={localisation}
-              onChange={(e) => handleChange(e)}
-              ref={register({
-                required: 'Veuillez remplir ce champ !',
-              })}
-            />
-            {errors.localisation && <div className="text-danger">{errors.localisation.message}</div>}
-          </Form.Group>
-          <Form.Group size="lg" controlId="categories">
-            <Form.Label>Style de votre voyage</Form.Label>
-            <div>
-            {categoriesList.map(category =>{
-              return <Form.Check
-              key={category.id}
-              type="checkbox"
-              label={category.entitled}
-              name="categories"
-              value={category.entitled}
-              onChange={(e) => handleCheckbox(e)}
-              ref={register({
-                required: 'Veuillez remplir ce champ !',
-              })}
-            />
-            })}
-           </div>
-            {errors.categories && <div className="text-danger">{errors.categories.message}</div>}
-          </Form.Group>
-          <Form.Group size="lg" controlId="departure">
-            <Form.Label>Date de départ</Form.Label>
-            <Form.Control
-              autoFocus
-              name="departure"
-              type="text"
-              defaultValue={departure}
-              onChange={(e) => handleChange(e)}
-              ref={register({
-                required: 'Veuillez remplir ce champ !',
-              })}
-            />
-            {errors.departure && <div className="text-danger">{errors.departure.message}</div>}
-          </Form.Group>
-          <Form.Group size="lg" controlId="returndate">
-            <Form.Label>Date de retour</Form.Label>
-            <Form.Control
-              autoFocus
-              name="returndate"
-              type="text"
-              defaultValue={returndate}
-              onChange={(e) => handleChange(e)}
-              ref={register({
-                required: 'Veuillez remplir ce champ !'
-              })}
-            />
-            {errors.returndate && <div className="text-danger">{errors.returndate.message}</div>}
-          </Form.Group>
-          {/* A la soumission du form, en attente de la réponse serveur le bouton est désactivé */}
-          <Button block size="lg" className="mt-3" type="submit" disabled={submitting}>
-            Valider
-          </Button>  
-      </Form>
-
+            </div>
+              {errors.categories && <div className="text-danger">{errors.categories.message}</div>}
+            </Form.Group>
+            <Form.Group size="lg" controlId="departure">
+              <Form.Label>Date de départ</Form.Label>
+              <Form.Control
+                name="departure"
+                type="date"
+                defaultValue={departure}
+                onChange={(e) => handleChange(e)}
+                ref={register({
+                  required: 'Veuillez remplir ce champ !',
+                })}
+              />
+              {errors.departure && <div className="text-danger">{errors.departure.message}</div>}
+            </Form.Group>
+            <Form.Group size="lg" controlId="returndate">
+              <Form.Label>Date de retour</Form.Label>
+              <Form.Control
+                name="returndate"
+                type="date"
+                defaultValue={returndate}
+                onChange={(e) => handleChange(e)}
+                ref={register({
+                  // tentative de vérification de la date de retour après le départ mais ça fonctionne pas
+                  // TODO: corriger cette vérif
+                  validate: value => new Date(value).getTime() < new Date(departure).getTime()
+                    
+                })}
+              />
+              {errors.returndate && <div className="text-danger">{errors.returndate.message}</div>}
+            </Form.Group>
+          </Col>
+          <Col lg={4} className="trip-preview-container"><AddTripPreview inputs={inputs}/></Col>
+            {/* A la soumission du form, en attente de la réponse serveur le bouton est désactivé */}
+            <Button size="lg" className="mt-3" type="submit" disabled={submitting}>
+              Valider
+            </Button>  
+          </Row> 
+        </Form>
+                 
     </Container>
-    <AddTripPreview />
   </div>
 }
 
