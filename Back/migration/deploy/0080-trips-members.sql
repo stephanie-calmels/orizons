da9c170b7ce2531e7e892dbeec693e8dcf40e81e
Armandine BARGE <armandine.barge@ntymail.com>
2021-01-28 14:26:45 +0100

    updating step datamapper

--- Back/migration/deploy/0080-trips-members.sql
+++ Back/migration/deploy/0080-trips-members.sql
@@ -2,7 +2,9 @@
 
 BEGIN;
 
-SELECT VIEW 
+ALTER TABLE "step" ADD COLUMN "step_date" DATE;
+
+CREATE VIEW "category_by_trip" AS
 SELECT "trip"."id", "title", JSON_AGG("category") FROM "trip"
 JOIN "_m2m_trip_category" tc ON "tc"."trip_id" = "trip"."id"
 JOIN "category" ON "category"."id" = "tc"."category_id"
@@ -10,12 +12,13 @@ GROUP BY "trip"."id", "title";
 
 
 
-SELECT nickname, biography, cover_member, profile_photo, registration_date, localisation,
-        JSON_AGG("trip")
-FROM member
-LEFT OUTER JOIN trip ON trip.member_id = member.id
-LEFT OUTER JOIN category_trip_id ON 
-GROUP BY nickname, biography, cover_member, profile_photo, registration_date, localisation;
+SELECT "nickname", "biography", "cover_member", "profile_photo", "registration_date", "localisation",
+        JSON_AGG("trip"),
+        JSON_AGG("category_by_trip")
+FROM "member"
+LEFT OUTER JOIN "trip" ON "trip"."member_id" = "member"."id"
+LEFT OUTER JOIN "category_by_trip" cbt ON cbt."id" = "trip"."id"
+GROUP BY "nickname", "biography", "cover_member", "profile_photo", "registration_date", "localisation";
 
 
 COMMIT;
