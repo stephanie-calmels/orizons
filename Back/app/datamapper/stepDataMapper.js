1df99f902563a6533eb820aef029fa507a54ebad
Armandine BARGE <armandine.barge@ntymail.com>
2021-02-01 19:00:52 +0100

    Fix new step 7

--- Back/app/datamapper/stepDataMapper.js
+++ Back/app/datamapper/stepDataMapper.js
@@ -23,7 +23,8 @@ const stepDataMapper = {
         console.log('nbstep', nbStep);
         // 2 - search if the pair trip/country exists en table m2m
         const tripCountry = await client.query(`SELECT tc."id" FROM "_m2m_trip_country" tc JOIN "country" ON "country"."id" = tc."country_id" WHERE tc."trip_id" = $1`, [newStep.trip_id])
-        console.log("2-1")
+        console.log("2-1");
+        const country
         if (!tripCountry.rows[0]) {
             const idCountry = await client.query(`SELECT "id" FROM "country", WHERE "code" = $1`, [newStep.country_code])
             await client.query(`INSERT INTO "_m2m_trip_country"("trip_id", "country_id") VALUES ($1, $2)`, [newStep.trip_id, idCountry.rows[0].id])
