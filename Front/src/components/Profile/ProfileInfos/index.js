ec86b10a620c9e0454d377551d91720cae1c4cca
stephanie-calmels <stephcalmels82@gmail.com>
2021-02-03 15:54:39 +0100

    MODIFY css profile_page 3

--- Front/src/components/Profile/ProfileInfos/index.js
+++ Front/src/components/Profile/ProfileInfos/index.js
@@ -24,11 +24,11 @@ const ProfileInfos = ({ member, trips, handleClick }) => {
       <Col className="stats-container">Statistiques
         <Container className="stats">
           <Row className="stats-row">
-            <Col lg={6} >{trips.length} carnets publiés</Col>
-            <Col lg={6}>12000km parcourus</Col>
+            <Col lg={6} ><i class="fas fa-book-open mr-2 stats-icons"></i>{trips.length} carnets publiés</Col>
+            <Col lg={6}><i class="fas fa-road mr-2 stats-icons"></i>12000km parcourus</Col>
           </Row>
           <Row className="stats-row">
-            <Col lg={4}>Pays visités</Col>
+            <Col lg={4}><i class="fas fa-flag mr-2 stats-icons"></i>Pays visités</Col>
             <Col lg={8}>
             {
               trips.map(trip => (
