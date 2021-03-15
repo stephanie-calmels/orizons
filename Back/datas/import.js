5f83a4bad67668a6682a0e2da500cffcd6025101
stephanie-calmels <stephcalmels82@gmail.com>
2021-03-15 12:25:16 +0100

    MODIFY SSL Client immport.js

--- Back/datas/import.js
+++ Back/datas/import.js
@@ -24,7 +24,9 @@ const countries = require('./import_country.json');
 (async () => {
     const client = new Client({
         connectionString: process.env.DATABASE_URL,
-        ssl: true
+        ssl: {
+            rejectUnauthorized: false
+        }
     });
     await client.connect();
 
